5152e216d68ef7c47a45f5308166ba3c26d51864
Fabio Medina <disenofabiomedina@gmail.com>
2025-05-26 21:07:07 -0500

    feat: :sparkles: Mejora la sección de sprites al agregar condiciones para mostrar sprites traseros

--- src/pokemons/components/SpritesSection.tsx
+++ src/pokemons/components/SpritesSection.tsx
@@ -10,30 +10,39 @@ export const SpritesSection = ({ sprites }: Props) => {
     <div className="flex flex-col w-full rounded-[10px] border-[1px] border-gray-300 bg-clip-border shadow-v shadow-gray-300 p-5">
       <h2 className="text-2xl font-bold mb-2">Sprites</h2>
       <div className="flex flex-row gap-2 flex-wrap items-center justify-center">
-        <Image
-          alt={`Sprite de Pokemon`}
-          width={100}
-          height={100}
-          src={`${sprites.back_default}`}
-        />
+
         <Image
           alt={`Sprite de Pokemon`}
           width={100}
           height={100}
           src={`${sprites.front_default}`}
         />
-        <Image
-          alt={`Sprite de Pokemon`}
-          width={100}
-          height={100}
-          src={`${sprites.back_shiny}`}
-        />
+        {
+          sprites.back_default && (
+            <Image
+              alt={`Sprite de Pokemon`}
+              width={100}
+              height={100}
+              src={`${sprites.back_default}`}
+            />
+          )
+        }
         <Image
           alt={`Sprite de Pokemon`}
           width={100}
           height={100}
           src={`${sprites.front_shiny}`}
         />
+        {
+          sprites.back_shiny && (
+            <Image
+              alt={`Sprite de Pokemon`}
+              width={100}
+              height={100}
+              src={`${sprites.back_shiny}`}
+            />
+          )
+        }
       </div>
     </div>
   )
