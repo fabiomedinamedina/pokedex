b519a5a0028678a5b42c189762959eb53f48d1fc
Fabio Medina <disenofabiomedina@gmail.com>
2025-06-01 21:17:44 -0500

    feat: :sparkles: Implementación de Redux (reducer) para el contador

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@/store';
+import { addOne, initiCounterState, resetCount, substractOne } from '@/store/counter/counterSlice';
+import { useEffect } from 'react';
 
 interface Props {
   value?: number
@@ -8,20 +10,25 @@ interface Props {
 
 export const CartCounter = ({ value = 0 }: Props) => {
 
-  const [counter, setCounter] = useState(value);
+  const count = useAppSelector(state => state.counter.count);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(initiCounterState(value));
+  }, [dispatch, value]);
 
   return (
     <>
-      <span className="text-9xl font-extrabold">{counter}</span>
+      <span className="text-9xl font-extrabold">{count}</span>
 
       <div className="flex gap-2">
         <button
-          onClick={() => setCounter( counter - 1 )}
+          onClick={() => dispatch(substractOne())}
           className="flex items-center justify-center p-2 rounded-xl bg-red-normal text-white hover:bg-yellow-normal transition-all, w-[100px]">
           -1
         </button>
         <button
-          onClick={() => setCounter( counter + 1 )}
+          onClick={() => dispatch(addOne())}
           className="flex items-center justify-center p-2 rounded-xl bg-red-normal text-white hover:bg-yellow-normal transition-all, w-[100px]">
           +1
         </button>
